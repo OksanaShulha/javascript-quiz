b41ef54ab4e27bd593fe68a910257c2bd7adf407
rafael garcia <rago89@gmail.com>
2021-07-08 17:41:48 +0200

    add format to files, and fix errors while running npm dependencies

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,8 +1,8 @@
-import { page } from './components/layout/page.js';
+import { page } from "./components/layout/page.js";
 
-import { home } from './components/pages/home/index.js';
-import { quiz } from './components/pages/quiz/index.js';
-import { questions } from './components/pages/questions/index.js';
+import { home } from "./components/pages/home/index.js";
+import { quiz } from "./components/pages/quiz/index.js";
+import { questions } from "./components/pages/questions/index.js";
 
 /**
  * Defines the route URLs, names and callbacks.
@@ -11,21 +11,21 @@ import { questions } from './components/pages/questions/index.js';
  */
 
 const parse = (params = {}) => {
-  if (params === null || typeof params !== 'object') {
+  if (params === null || typeof params !== "object") {
     return {};
   }
   return Object.fromEntries(
     Object.entries(params)
-      .filter((param) => param[1] !== 'undefined')
+      .filter((param) => param[1] !== "undefined")
       .map((param) => [
         param[0],
-        param[1] !== '' && !Number.isNaN(Number(param[1]))
+        param[1] !== "" && !Number.isNaN(Number(param[1]))
           ? Number(param[1])
-          : param[1] === 'true'
+          : param[1] === "true"
           ? true
-          : param[1] === 'false'
+          : param[1] === "false"
           ? false
-          : param[1] === 'null'
+          : param[1] === "null"
           ? null
           : param[1],
       ])
@@ -39,25 +39,25 @@ const routeHandler =
   ({ params, data }) => {
     const namedRoutes = routes.filter((route) => `name` in route);
     const cleanData = { ...data };
-    cleanData.id = !data || !('id' in data) ? -1 : data.id;
-    const root = document.getElementById('root');
-    root.innerHTML = '';
+    cleanData.id = !data || !("id" in data) ? -1 : data.id;
+    const root = document.getElementById("root");
+    root.innerHTML = "";
     root.appendChild(page(pageBody(parse(data), parse(params)), namedRoutes));
   };
 
 [
   {
-    name: 'home',
+    name: "home",
     path: `/`,
     callback: routeHandler(home),
   },
   {
-    name: 'quiz',
+    name: "quiz",
     path: `/quiz`,
     callback: routeHandler(quiz),
   },
   {
-    name: 'questions',
+    name: "questions",
     path: `/questions`,
     callback: routeHandler(questions),
   },
