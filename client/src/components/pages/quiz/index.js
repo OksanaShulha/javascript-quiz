6f5337bfeb420c2d646f176822d4ffb0dd84e13b
OksanaShulha <shushullja@gmail.com>
2021-07-10 19:43:58 +0200

    Add toggle component

--- client/src/components/pages/quiz/index.js
+++ client/src/components/pages/quiz/index.js
@@ -1,4 +1,5 @@
-import { inputGreeting } from '../../shared/input-greeting.js';
+/* eslint-disable no-undef */
+import { inputGreeting } from "../../shared/input-greeting.js";
 
 /**
  * The quiz page.
@@ -6,10 +7,22 @@ import { inputGreeting } from '../../shared/input-greeting.js';
  * @returns {HTMLDivElement} A rendered quiz page.
  */
 export const quiz = () => {
-  const container = document.createElement('div');
-  container.className = 'body';
+  const container = document.createElement("div");
+  container.className = "body";
 
   container.appendChild(inputGreeting());
 
   return container;
 };
+
+// Toggle Class Component
+
+const toggleClass = (obj, newClass) => {
+  obj.classList[0].remove();
+  obj.classList.add(newClass);
+};
+if (answer === true){
+  toggleClass(target.green);
+} else {
+  toggleClass(target.red);
+}
