7d727eb8a9ca852ad3bbb177d914ed906ddcd376
OksanaShulha <shushullja@gmail.com>
2021-07-10 21:26:52 +0200

    Update toggle

--- client/src/components/pages/quiz/index.js
+++ client/src/components/pages/quiz/index.js
@@ -16,15 +16,19 @@ export const quiz = () => {
 
 // Toggle Class Component
 
+// eslint-disable-next-line no-unused-vars
 const toggleClass = (obj, newClass) => {
   obj.classList[0].remove();
   obj.classList.add(newClass);
+  // eslint-disable-next-line spellcheck/spell-checker
+  // eslint-disable-next-line no-undef
+  if (answer === true) {
+    // eslint-disable-next-line spellcheck/spell-checker
+    // eslint-disable-next-line no-undef
+    toggleClass(target.green);
+  } else {
+    // eslint-disable-next-line spellcheck/spell-checker
+    // eslint-disable-next-line no-undef
+    toggleClass(target.red);
+  }
 };
-// eslint-disable-next-line no-undef
-if (answer === true){
-  // eslint-disable-next-line no-undef
-  toggleClass(target.green);
-} else {
-  // eslint-disable-next-line no-undef
-  toggleClass(target.red);
-}
