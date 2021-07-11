0c41458d6580ffe0a3e4b37f2f3b4dd22b73db0c
OksanaShulha <shushullja@gmail.com>
2021-07-11 12:49:46 +0200

    Update progressDot

--- client/src/components/pages/quiz/progress-bar.js
+++ client/src/components/pages/quiz/progress-bar.js
@@ -1,15 +1,11 @@
 // We have to import something
 
-export const progressBar = (indicatorClass, answer) => {
+export const progressDot = (indicatorClass, answer) => {
   const div = document.createElement("div");
   div.className = indicatorClass;
-  // we have to add some code to show that circles will be added one by one
-  if (answer === true) {
-    // eslint-disable-next-line no-undef
-    indicatorClass(target.green);
-  } else {
-    // eslint-disable-next-line no-undef
-    indicatorClass(target.red);
-  }
-  return div;
+  questions.forEach(() => {
+    progressDot.appendChild(div);
+  });
 };
+
+// not yet
