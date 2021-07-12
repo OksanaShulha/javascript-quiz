31b68744210dfe3681f70a9264fb25e9805aa99e
sy <sabanyelkenci@gmail.com>
2021-07-12 15:22:48 +0200

    add button component

---
+++ client/src/components/shared/button.js
@@ -0,0 +1,8 @@
+export const button = (text, handler) => {
+  const btn = document.createElement("button");
+  btn.className = "btn";
+  btn.addEventListener("click", handler);
+  btn.innerText = text;
+
+  return btn;
+};
