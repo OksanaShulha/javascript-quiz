3d643ed6830a05ac96702355c81aaa0c2ac9438b
OksanaShulha <shushullja@gmail.com>
2021-07-14 15:35:33 +0200

    Return back to quiz index.js and title.html

---
+++ client/src/components/pages/quiz/index.js
@@ -0,0 +1,15 @@
+import { inputGreeting } from "../../shared/input-greeting.js";
+
+/**
+ * The quiz page.
+ *
+ * @returns {HTMLDivElement} A rendered quiz page.
+ */
+export const quiz = () => {
+  const container = document.createElement("div");
+  container.className = "body";
+
+  container.appendChild(inputGreeting());
+
+  return container;
+};
