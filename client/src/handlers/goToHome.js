e8438a0d0a7f384c631daa101bbd8c2b26e13f4e
rafael garcia <rago89@gmail.com>
2021-07-15 14:58:18 +0200

    add go to home handler

--- client/src/handlers/goToHome.js
+++ client/src/handlers/goToHome.js
@@ -1,6 +1,27 @@
-// import { state } from "../init/state.js";
-// import { router } from "../init/router.js";
+import { state } from "../init/state.js";
+// eslint-disable-next-line import/no-cycle
+import { router } from "../init/router.js";
 
 export const goToHome = () => {
-  console.log("hello");
+  state.currentQuestion = 0;
+  for (let i = 0; i < state.questions.length; i++) {
+    // we loop through all questions
+    const question = state.questions[i]; // we get one specific question
+    question.answered = false; // user hasn't answered to any question
+    for (let j = 0; j < question.answers.length; j++) {
+      const answerOfQuestion = question.answers[j]; // we get one specific answer
+      answerOfQuestion.selected = false;
+    }
+  }
+  // clean the results
+  for (const key in state.results) {
+    if (key === "totalQuestions") {
+      // eslint-disable-next-line no-continue
+      continue;
+    }
+    state.results[key] = 0;
+  }
+
+  router.navigate("");
+  console.log(state);
 };
