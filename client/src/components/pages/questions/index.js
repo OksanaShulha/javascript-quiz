907415872a3beb089b5d0296f30980fc1d846110
rafael garcia <rago89@gmail.com>
2021-07-16 18:59:27 +0200

    add descriptionRange and question page

--- client/src/components/pages/questions/index.js
+++ client/src/components/pages/questions/index.js
@@ -1,4 +1,10 @@
-import { inputGreeting } from "../../shared/input-greeting.js";
+import { state } from "../../../init/state.js";
+import { button } from "../../shared/button.js";
+import { questionWithOptions } from "../../shared/questionWithOptions.js";
+// eslint-disable-next-line import/no-cycle
+import { goToHome } from "../../../handlers/goToHome.js";
+
+// import { state } from "remark";
 
 /**
  * The questions page.
@@ -6,10 +12,29 @@ import { inputGreeting } from "../../shared/input-greeting.js";
  * @returns {HTMLDivElement} A rendered questions page.
  */
 export const questions = () => {
+  // count all the questions in state.js
+  const { questions } = state;
+  state.results.totalQuestions = questions.length;
+  const { totalQuestions } = state.results;
+  // is going to count on each iteration rendering all the questions
+  let counter = 0;
+  // body container
   const container = document.createElement("div");
-  container.className = "body";
+  container.classList.add("quiz-box", "custom-box");
+  container.id = "body";
+  // render all questions
+  if (!document.getElementById("question")) {
+    state.questions.forEach((questionObj) => {
+      counter++;
+      const currentQuestion = counter;
+      container.appendChild(
+        questionWithOptions(questionObj, currentQuestion, totalQuestions)
+      );
+    });
+  }
 
-  container.appendChild(inputGreeting());
+  const returnButton = button("Go To Home", goToHome);
+  container.appendChild(returnButton);
 
   return container;
 };
