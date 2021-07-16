a999092460225e233f5a8dcac32c5eadef12b164
rafael garcia <rago89@gmail.com>
2021-07-17 01:52:38 +0200

    change range to %  resultDescription component

--- client/src/components/layout/resultsDescriptionRange.js
+++ client/src/components/layout/resultsDescriptionRange.js
@@ -1,17 +1,18 @@
 import { titleDescription } from "../shared/title-description.js";
 
 // eslint-disable-next-line consistent-return
-export const resultDescriptionRange = (correctAnswer) => {
-  if (correctAnswer <= 2) {
+export const resultDescriptionRange = (correctAnswer, totalQuestions) => {
+  const percentage = (correctAnswer / totalQuestions) * 100;
+  if (percentage <= 50) {
     return titleDescription("Results", "Keep learning and try again later!");
   }
-  if (correctAnswer === 3) {
+  if (percentage > 50 && percentage <= 65) {
     return titleDescription("Results", "Good but you can get better!");
   }
-  if (correctAnswer === 4) {
+  if (percentage > 65 && percentage <= 80) {
     return titleDescription("Results", "Great job, almost perfect!");
   }
-  if (correctAnswer === 5) {
+  if (percentage > 80 && percentage <= 100) {
     return titleDescription("Results", "Excellent you are hired!");
   }
 };
